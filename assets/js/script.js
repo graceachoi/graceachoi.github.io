74414250ad974781b01ed7b584243b530bbe4036
myung-eun <gracechoi@Graces-MacBook-Pro-4.local>
2017-03-31 20:37:42 -0700

    First

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+
+//CONSOLE LOG
+// $(document).scroll(function() {
+//    console.log($(window).scrollTop());
+// });
+
+$(".introtitle").click(function() {
+  $('.aboutbox').show();
+});
+
+
+});
