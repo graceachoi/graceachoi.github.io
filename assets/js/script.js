72373854085f6f716186a3b659553d5aa20325ff
myung-eun <gracechoi@Graces-MacBook-Pro-4.local>
2017-04-02 10:32:10 -0700

    6

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,29 @@ $(document).ready(function() {
 //    console.log($(window).scrollTop());
 // });
 
-
-
-$(".link.2").click(function() {
-  $('.aboutbox').show();
-
+$(function() {
+   $('body').removeClass('fade-out');
 });
+
+$(".linkcontact").click(function() {
+  $('.section2.contact').show();
+});
+
+
+
+$('.link.contact').click(function(e){
+    $('.section.about').fadeOut('fast', function(){
+        $('.section2.contact').fadeIn('fast');
+    });
+});
+
+
+$('.link.about').click(function(e){
+	$('.section2.contact').fadeOut('fast', function(){
+		$('.section.about').fadeIn('fast');
+	});
+});
+
 
 
 });
